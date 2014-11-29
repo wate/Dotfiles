ebcea5e0a9fe38e298790ee1e03d1029d2f22ed9
wate <pc.poisoning@gmail.com>
2014-11-30 04:03:07 +0900

    bash-completion用の補完ファイルの読み込み処理を追記

--- _bashrc.sh
+++ _bashrc.sh
@@ -1,2 +1,12 @@
 alias ls='ls -GF'
 alias ll='ls -GFAl'
+
+if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
+    source `brew --prefix`/etc/bash_completion.d/git-completion.bash
+fi
+if [ -f `brew --prefix`/etc/bash_completion.d/vagrant ]; then
+    source `brew --prefix`/etc/bash_completion.d/vagrant
+fi
+if [ -f `brew --prefix`/etc/bash_completion.d/wp ]; then
+    source `brew --prefix`/etc/bash_completion.d/wp
+fi
