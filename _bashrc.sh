7f9f158d36336a66d25574dc161779c3363241a6
wate <pc.poisoning@gmail.com>
2014-11-30 11:09:43 +0900

    update

--- _bashrc.sh
+++ _bashrc.sh
@@ -3,12 +3,19 @@ alias ll='ls -GFAl'
 
 source $(brew --repository)/Library/Contributions/brew_bash_completion.sh
 
+# Git
 if [ -f `brew --prefix`/etc/bash_completion.d/git-completion.bash ]; then
     source `brew --prefix`/etc/bash_completion.d/git-completion.bash
 fi
+# Git Flow
+if [ -f `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash ]; then
+    source `brew --prefix`/etc/bash_completion.d/git-flow-completion.bash
+fi
+# Vagrant
 if [ -f `brew --prefix`/etc/bash_completion.d/vagrant ]; then
     source `brew --prefix`/etc/bash_completion.d/vagrant
 fi
+# WP-CLI
 if [ -f `brew --prefix`/etc/bash_completion.d/wp ]; then
     source `brew --prefix`/etc/bash_completion.d/wp
 fi
