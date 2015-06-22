bf206c4f62e7a3effa2ec2f75d23b6cdb1e5db1b
wate <pc.poisoning@gmail.com>
2015-06-23 03:20:21 +0900

    update

--- _bash_profile.sh
+++ _bash_profile.sh
@@ -2,9 +2,8 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-PATH=$(brew --prefix homebrew/php/php55)/bin:$PATH:$HOME/.composer/vendor/bin
+PATH=$HOME/.nodebrew/current/bin:$(brew --prefix homebrew/php/php55)/bin:$PATH:$HOME/.composer/vendor/bin
 export PATH
-
 export ANDROID_HOME=/usr/local/opt/android-sdk
 
 # gitignore.io
