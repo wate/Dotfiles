8a911298127e3aca59a63c7fa3e9c883985ce745
wate <pc.poisoning@gmail.com>
2015-08-23 20:38:58 +0900

    APIキーの情報などを記載しているファイルを読み込むように調整

--- _bash_profile.sh
+++ _bash_profile.sh
@@ -6,5 +6,9 @@ PATH=$HOME/.nodebrew/current/bin:$(brew --prefix homebrew/php/php55)/bin:$PATH:$
 export PATH
 export ANDROID_HOME=/usr/local/opt/android-sdk
 
+if [ -f ~/.secret ]; then
+    source ~/.secret
+fi
+
 # gitignore.io
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
