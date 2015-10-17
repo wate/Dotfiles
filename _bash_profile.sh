2399a624a30b02ade2eaeaf963c830768b266e77
wate <pc.poisoning@gmail.com>
2015-10-18 03:33:24 +0900

    パスを通す場所を調整

--- _bash_profile.sh
+++ _bash_profile.sh
@@ -2,7 +2,7 @@ if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 
-PATH=$HOME/.nodebrew/current/bin:$(brew --prefix homebrew/php/php55)/bin:$PATH:$HOME/.composer/vendor/bin
+PATH=$HOME/.nodebrew/current/bin:$(brew --prefix)/sbin:$PATH:$HOME/.composer/vendor/bin
 export PATH
 export ANDROID_HOME=/usr/local/opt/android-sdk
 
