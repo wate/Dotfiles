3a941c598306120a107e7a51d966b82d4fcbbb89
wate <pc.poisoning@gmail.com>
2015-11-19 04:57:34 +0900

    設定調整

--- _bashrc.sh
+++ _bashrc.sh
@@ -53,6 +53,10 @@ fi
 if [ -f "${BREW_PREFIX}"/etc/bash_completion.d/packer ]; then
     source "${BREW_PREFIX}"/etc/bash_completion.d/packer
 fi
+# pandoc
+if [ -f "${BREW_PREFIX}"/etc/bash_completion.d/pandoc ]; then
+    source "${BREW_PREFIX}"/etc/bash_completion.d/pandoc
+fi
 # aws cli
 if command_exists aws ; then
     complete -C aws_completer aws
